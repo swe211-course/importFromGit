887a31c750143da8301571bf4502c861b5ab93b7
swe211-course <ali-ccis@pokemail.net>
2016-12-27 21:13:14 +0300

    Create shop.java

---
+++ shop.java
@@ -0,0 +1,12 @@
+import java.util.Enumeration;
+
+
+public class eCommerce {
+
+public String name;
+public int cost; 
+
+public eCommerce(String name, int cost){
+	this.name =name;
+	this.cost=cost;
+}
